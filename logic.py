cb27dcf88de30466d23d18206dffb8b87d7cf555
Jad Shaheen <shaheenjad@gmail.com>
2022-12-27 22:27:23 -0500

    Returning sanitized input from sanitize_input and using in retrieve_search_results

--- logic.py
+++ logic.py
@@ -9,15 +9,15 @@ from utils.types import Table
 def retrieve_search_results(search_query):
 
 	if search_query:
-		input_type = sanitize_input(search_query.lower())
+		input_type, sanitized_input = sanitize_input(search_query)
 		if input_type == Table.PLAYER:
-			return Table.PLAYER.name, scraper.construct_players_map()[search_query.lower()]
+			return Table.PLAYER.name, scraper.construct_players_map()[sanitized_input]
 		elif input_type == Table.TOURNAMENT:
 			mens, womens = scraper.construct_nested_data_map(Table.TOURNAMENT)
-			return Table.TOURNAMENT.name, mens[match_tournament(search_query.lower())], womens[match_tournament(search_query.lower())]
+			return Table.TOURNAMENT.name, mens[match_tournament(sanitized_input)], womens[match_tournament(sanitized_input)]
 		elif input_type == Table.YEAR:
 			mens, womens = scraper.construct_nested_data_map(Table.YEAR)
-			return Table.YEAR.name, mens[search_query], womens[search_query]
+			return Table.YEAR.name, mens[sanitized_input], womens[sanitized_input]
 		elif input_type == Table.RANKINGS:
 			return Table.RANKINGS.name, scraper.get_rankings_table()
 	return "Input not understood. Please try again."
@@ -26,21 +26,25 @@ def retrieve_search_results(search_query):
 # method to figure out input type (player name, tournament name or year)
 # and normalize the input. Used to determine which logic flow to follow.
 def sanitize_input(input):
-	if re.fullmatch(r'rank(ing[s]?)?', input):
-		return Table.RANKINGS
-	elif match_tournament(input):
-		return Table.TOURNAMENT
-	elif input.isnumeric():
-		return Table.YEAR
+	input_type = None
+	sanitized_input = input.lower()
+	matched_tournament = match_tournament(input)
+	if re.fullmatch(r'rank(ing[s]?)?', sanitized_input):
+		input_type = Table.RANKINGS
+	elif matched_tournament:
+		input_type = Table.TOURNAMENT
+		sanitized_input = matched_tournament
+	elif sanitized_input.isnumeric():
+		input_type = Table.YEAR
 	# TODO: Figure out case here if not likely player name to return something like,
 	# 'input not a recognized player/tournament/year'
 	else:
-		return Table.PLAYER
+		input_type = Table.PLAYER
+	return input_type, sanitized_input
 
 def match_tournament(tournament_name):
 	tournament_patterns_map = {r'u[.]?s[.]? open': 'U.S. Open', r'wimbledon': 'Wimbledon', r'french open|roland garros': 'French Open', r'aus(tralian|sie) open': 'Australian Open'}
 	for pattern in tournament_patterns_map.keys():
 		if re.fullmatch(pattern, tournament_name):
-			print(tournament_patterns_map[pattern])
 			return tournament_patterns_map[pattern]
 	return False
